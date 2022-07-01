e76aff964cf7a64e401d9484c875b0afbd4d616c
Jinsu Kwak <jinsukwak2001@gmail.com>
2022-07-01 03:03:30 -0600

    Fix minor bugs in mobile viewport

--- JS/script.js
+++ JS/script.js
@@ -988,17 +988,18 @@ function values(reason) {
 /* Education animation  (mobile) */
 $(document).ready(function () {
   $(".eduyear-mobile>.ed-yearnum--mobile").click(function () {
-    $(this).next("ul").toggleClass("hide");
     var submenu = $(this).next("ul");
     if (submenu.is(":visible")) {
+      $(this).next("ul").addClass("hide");
       submenu.slideUp();
       $(this).removeClass("pinkborder");
-      $(this).children("ion-icon").css("transform", "rotate(0deg)");
+      $(this).children("ion-icon").css("transform", "scaleY(1)");
       $(this).children("ion-icon").css("transition-duration", "0.3s");
     } else {
       submenu.slideDown();
+      $(this).next("ul").removeClass("hide");
       $(this).addClass("pinkborder");
-      $(this).children("ion-icon").css("transform", "rotate(-180deg)");
+      $(this).children("ion-icon").css("transform", "scaleY(-1)");
       $(this).children("ion-icon").css("transition-duration", "0.3s");
     }
   });
